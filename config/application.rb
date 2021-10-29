773ceb714f8d02d0368c07555a4b4f31af4afe5c
kokoriru <yuka@ryukalice.com>
2021-10-29 09:00:49 +0900

    locale 設定

--- config/application.rb
+++ config/application.rb
@@ -21,18 +21,15 @@ Bundler.require(*Rails.groups)
 
 module LongTermMissions
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.i18n.available_locales = %i[en ja]
+    config.time_zone = 'Tokyo'
+
     # config.eager_load_paths << Rails.root.join("extras")
 
-    # Don't generate system test files.
     config.generators.system_tests = nil
   end
 end
